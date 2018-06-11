21a7488baf4a4fd7e314a5c268adc58d14e7dc5c
Andrew Madsen <andrew@openreelsoftware.com>
2018-06-11 09:48:42 -0600

    Add solution project

---
+++ Rainbow/Sources/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by Andrew R Madsen on 6/11/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func makeBackgroundGreen(_ sender: Any) {
+        view.backgroundColor = .green
+    }
+    
+    @IBAction func makeBackgroundYellow(_ sender: Any) {
+        view.backgroundColor = .yellow
+    }
+    
+    @IBAction func makeBackgroundOrange(_ sender: Any) {
+        view.backgroundColor = .orange
+    }
+
+    @IBAction func makeBackgroundRed(_ sender: Any) {
+        view.backgroundColor = .red
+    }
+    
+    @IBAction func makeBackgroundPurple(_ sender: Any) {
+        view.backgroundColor = .purple
+    }
+    
+    @IBAction func makeBackgroundBlue(_ sender: Any) {
+        view.backgroundColor = .blue
+    }
+}
+
