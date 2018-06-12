a25030a1eb25407d42e8f388e3ba02c3787d904a
AJ Genung <DragonRydrz@me.com>
2018-06-12 01:06:12 -0400

    AJ Genung -- completed

---
+++ Rainbow/Rainbow/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by AJ Genung on 6/12/18.
+//  Copyright © 2018 AJ Genung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func turnGreen(_ sender: Any) {
+        view.backgroundColor = .green
+    }
+    @IBAction func turnYellow(_ sender: Any) {
+        view.backgroundColor = .yellow
+    }
+    @IBAction func turnOrange(_ sender: Any) {
+        view.backgroundColor = .orange
+    }
+    @IBAction func turnRed(_ sender: Any) {
+        view.backgroundColor = .red
+    }
+    @IBAction func turnPurple(_ sender: Any) {
+        view.backgroundColor = .purple
+    }
+    @IBAction func turnBlue(_ sender: Any) {
+        view.backgroundColor = .blue
+    }
+    
+}
+
